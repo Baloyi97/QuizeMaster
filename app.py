b0a7d67909cf6a208803d0d8da7acf5864342e8d
Baloyi97 <vernonbaloyi41@gmail.com>
2024-07-10 22:15:51 +0200

    updating app

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_mail import Mail, Message
+import os
 from flask_bcrypt import Bcrypt
 from datetime import timedelta
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecretkey'
@@ -13,6 +18,18 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'index'
 login_manager.remember_cookie_duration = timedelta(days=7)  # Optional: set duration for remember me
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'  # or your mail server
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('vernonbaloyi41@gmail.com')  # replace with your email
+app.config['MAIL_PASSWORD'] = os.environ.get('wgyd qera uolp ssqt')  # replace with your email password
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('vernonbaloyi41@gmail.com')  # ensure this matches the authenticated email
+
+
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'index'
+mail = Mail(app)
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -68,6 +85,24 @@ def logout():
 @app.route("/terms")
 def terms():
     return render_template('terms.html')
+
+@app.route("/forgot_password", methods=['GET', 'POST'])
+def forgot_password():
+    if request.method == 'POST':
+        email = request.form['email']
+        user = User.query.filter_by(email=email).first()
+        if user:
+            msg = Message('Your QuizMaster Password',
+                          sender='noreply@demo.com',
+                          recipients=[email])
+            msg.body = f'Your password is: {user.password}'
+            mail.send(msg)
+            flash('An email with your password has been sent!', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Email address not found', 'danger')
+    return render_template('forgot_password.html')
+
 
 
 if __name__ == '__main__':
